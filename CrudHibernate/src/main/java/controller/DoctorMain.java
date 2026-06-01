ab2aeb855dd3cc7812ecafa3f99384d0380b9f91
Govind Dangi <govinddangi580@gmail.com>
2026-06-01 19:44:55 +0530

    HQL

--- CrudHibernate/src/main/java/controller/DoctorMain.java
+++ CrudHibernate/src/main/java/controller/DoctorMain.java
@@ -1,11 +1,13 @@
 package controller;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import dto.DoctorInfo;
 import entity.Doctor;
 import entity.Gender;
 
@@ -16,15 +18,25 @@ public class DoctorMain {
 		Session s = sf.openSession();
 		s.beginTransaction();
 
-		Doctor d = new Doctor("Jay", 21, LocalDate.of(2003, 5, 22), 101, "Heart", Gender.Male, "jay12@gmail.com",
-				"7896321455");
-		
-		s.persist(d);
-		
+//		Doctor d = new Doctor("Kartik", 22, LocalDate.of(2004, 5, 28), 104, "Nothing", Gender.Male, "Kartik12@gmail.com",
+//				"7897378455");
+//		
+//		s.persist(d);
+
 //		Doctor d = s.get(Doctor.class, 3);
 //		s.remove(d);
+
+//		List<Doctor> list = s.createQuery("from Doctor", Doctor.class).list();
+//		list.stream().forEach(a -> System.out.println(a));
+
+//		List<Doctor> list = s.createQuery("from Doctor where name like '%Jay%'", Doctor.class).list();
+//		list.stream().forEach(a -> System.out.println(a));
 		
-		
+		List<DoctorInfo> list = s.createQuery("select name, phone, email from Doctor", DoctorInfo.class).list();
+		list.stream().forEach(a -> System.out.println(a));
+
+//		List<Doctor> list = s.createQuery("from Doctor order by name desc", Doctor.class).list();
+//		list.stream().forEach(a -> System.out.println(a));
 
 		s.getTransaction().commit();
 
---
+++ CrudHibernate/src/main/java/dto/DoctorInfo.java
@@ -0,0 +1,5 @@
+package dto;
+
+public record DoctorInfo(String name, String phone, String email) {
+
+}
