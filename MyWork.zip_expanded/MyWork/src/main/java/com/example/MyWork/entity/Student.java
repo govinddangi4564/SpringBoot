26752376135fbce58ae2107ce975cb889cdab645
Govind Dangi <govinddangi580@gmail.com>
2026-07-13 15:33:14 +0530

    UniDirectional One to One Mapping

--- MyWork.zip_expanded/MyWork/src/main/java/com/example/MyWork/entity/Student.java
+++ MyWork.zip_expanded/MyWork/src/main/java/com/example/MyWork/entity/Student.java
@@ -1,10 +1,13 @@
 package com.example.MyWork.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,6 +31,8 @@ public class Student {
 
 	@Column(unique = true)
 	private String phone;
-	private String address;
-	private String course;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id")
+	private Address address;
 }
